9d3085720ce0c445610072816f9073ce46bf0b5e
khalil-mejri-1 <mejrik1888@gmail.com>
2026-02-21 03:08:23 +0100

    final update-9 2/21/2026

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -8,7 +8,7 @@
 // const API_BASE_URL = 'https://skygros-nifd.vercel.app/api';
 
 // للاستخدام مع Proxy (الإعداد الحالي):
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = '/api';
 
 // تصدير API_BASE_URL كـ default export
 export default API_BASE_URL;
