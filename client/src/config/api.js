5cc007d6b3c37453be5f5624d93f4dc7655a462d
khalil-mejri-1 <mejrik1888@gmail.com>
2026-03-18 16:13:32 +0100

    final update-9 3/18/2026

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,5 +1,7 @@
-const API_BASE_URL = 'https://skygros.vercel.app/api';
+// const API_BASE_URL = 'https://skygros.vercel.app/api';
 // const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://skygros.com/api';
+
 
 export const formatImageUrl = (url) => {
     if (!url) return '';
