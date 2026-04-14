b5e78605bd729613739e70a7bf286403b58f61f9
khalil-mejri-1 <mejrik1888@gmail.com>
2026-04-14 09:45:24 +0100

    final update-9 4/1/2026

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,5 +1,5 @@
-const API_BASE_URL = 'http://localhost:5000/api';
-// const API_BASE_URL = 'https://skygros.com/api';
+//const API_BASE_URL = 'http://localhost:5000/api';
+ const API_BASE_URL = 'https://skygros.com/api';
 
 
 export const formatImageUrl = (url) => {
